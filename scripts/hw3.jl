b4b8265c30a457dbb0186be62c010862fe1d2126
Son <snpham02@gmail.com>
2024-02-20 20:27:09 -0600

    q2 cleanup, adding q3

--- scripts/hw3.jl
+++ scripts/hw3.jl
@@ -3,7 +3,7 @@ using POMDPs: actions, @gen, isterminal, discount, statetype, actiontype, simula
 using D3Trees: inchrome, inbrowser
 using StaticArrays: SA
 using Statistics: mean
-using BenchmarkTools: @btime
+using BenchmarkTools: @btime, @benchmark
 using Statistics
 
 ##############
@@ -24,19 +24,10 @@ Please make sure to update DMUStudent to gain access to the HW3 module.
 # Question 2
 ############
 
-m = HW3.DenseGridWorld(seed=3)
-
-As = actions(m)
-# isterminal(m, s)
-γ = discount(m)
-S_type = statetype(m)
-A_type = actiontype(m)
-
-
-function rollout(mdp, policy_function, s0, max_steps=100)
+function rollout_q2(mdp, policy_function, s0, max_steps=100)
     # fill this in with code from the assignment document
     r_total = 0.0 
-    t=0 
+    t = 0 
     s = s0
 
     while !isterminal(mdp, s) && t < max_steps
@@ -71,14 +62,120 @@ function heuristic_policy(m, s)
     end
 end
 
+m = HW3.DenseGridWorld(seed=3)
 # This code runs monte carlo simulations: you can calculate the mean and standard error from the results
-results = [rollout(m, random_policy, rand(initialstate(m))) for _ in 1:500]
-# @show mean_reward = mean(results)
-# @show SEM = std(results) / sqrt(length(results))
+results = [rollout_q2(m, random_policy, rand(initialstate(m))) for _ in 1:500]
+@show mean_reward = mean(results)
+@show SEM = std(results) / sqrt(length(results))
 
+m = HW3.DenseGridWorld(seed=3)
 # This code runs monte carlo simulations: you can calculate the mean and standard error from the results
-results = [rollout(m, heuristic_policy, rand(initialstate(m))) for _ in 1:500]
+results = [rollout_q2(m, heuristic_policy, rand(initialstate(m))) for _ in 1:500]
 @show mean_results = mean(results)
 @show SEM = std(results) / sqrt(length(results))
 
+
+
+
+
+
+
+############
+# Question 3
+############
+
+function select_action(mdp, s, d, π₀, Q, N, T, t)
+    simulate!(mdp, s, d, π₀, Q, N, T, t)
+    return explore(mdp, s, Q, N)
+end
+
+function simulate!(mdp, s, d, π₀, Q, N, T, t)
+    if d == 0 || isterminal(mdp, s)
+        return 0
+    end
+
+    if s ∉ T
+        for a in actions(mdp)
+            N[(s, a)] = 0
+            Q[(s, a)] = 0.0
+        end
+        push!(T, s)
+        return rollout_loop(mdp, s, d, π₀)
+    end
+
+    a = explore(mdp, s, Q, N)
+    sp, r = @gen(:sp,:r)(mdp, s, a)
+
+    t_key = (s, a, sp)
+    t[t_key] = get(t, t_key, 0) + 1
+
+    q = r + discount(m) * simulate!(mdp, sp, d - 1, π₀, Q, N, T, t)
+    N[(s, a)] += 1
+    Q[(s, a)] += (q - Q[(s, a)]) / N[(s, a)]
+    return q
+end
+
+function rollout_recursive(mdp, s, d, π₀)
+    if d == 0 || isterminal(mdp, s)
+        return 0
+    end
+    a = π₀(mdp, s)
+    sp, r = @gen(:sp,:r)(mdp, s, a) 
+    return r + discount(m) * rollout_recursive(mdp, sp, d - 1, π₀)
+end
+
+function rollout_loop(mdp, s, d, π₀)
+    r_total = 0.0
+    discount_factor = 1.0
+    current_state = s
+    
+    for current_depth in 1:d
+        if isterminal(mdp, current_state)
+            break
+        end
+        
+        a = π₀(mdp, current_state)
+        sp, r = @gen(:sp, :r)(mdp, current_state, a)
+        
+        r_total += discount_factor * r
+        discount_factor *= discount(mdp)
+        current_state = sp
+    end
+    
+    return r_total
+end
+
+function explore(mdp, s, Q, N)
+    c = sqrt(2)
+    As = actions(mdp, s)
+    
+    if all(a -> (s, a) ∉ keys(N), As)
+        return rand(As)
+    end
+
+    # total visits
+    Ns = sum([N[(s, a)] for a in As if (s, a) in keys(N)])
+
+    ucb_values = [(a, ((s, a) in keys(N) && N[(s, a)] > 0) ? 
+        Q[(s, a)] / N[(s, a)] + c * sqrt(2 * log(Ns) / N[(s, a)]) : Inf) for a in As]
+
+    return maximum(ucb_values)[1]
+end
+
+# running q3
+m = DenseGridWorld(seed=4)
+n = Dict{Tuple{statetype(m), actiontype(m)}, Int}()
+q = Dict{Tuple{statetype(m), actiontype(m)}, Float64}()
+T = Set{statetype(m)}()
+t = Dict{Tuple{statetype(m), actiontype(m), statetype(m)}, Int}()
+
+# Example usage:
+s0 = SA[19, 19]
+max_depth = 7
+max_iters = 7
+π₀ = heuristic_policy
+    for i in 1:max_iters
+        select_action(m, s0, max_depth, π₀, q, n, T, t)
+    end
+inchrome(visualize_tree(q, n, t, s0))
 
